3bd58907c79e9990d917ec091fba8d8dfb0a3c02
Sagona2214 <sagona2214@gmail.com>
2025-06-11 17:54:30 +0530

    Add files via upload

---
+++ Variable.py
@@ -0,0 +1,16 @@
+'''lunchBox="pasta"
+print(lunchBox)
+age=20
+print(age)
+MyName="sagona"
+_myname= "coding"
+print(MyName)
+print(_myname)
+age=20
+print(Age)
+MyName="sagona"#pascal
+myName="sagona"#camel
+my_name="sagona"#snake
+myname="sagona"
+x,y,z=12,4,6
+print(x,y,z)'''
