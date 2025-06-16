1cfac8378da81aa114d8267498c7125218055212
Sagona2214 <sagona2214@gmail.com>
2025-06-16 18:07:58 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,37 @@
+
+a=3
+b=4
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a**b)
+
+x = 5
+x**=3 # x= x**3, x = 5**3 = 125
+print(x)
+x =2
+y=2
+print(x==y)
+print(x!=y)
+print(x>y)
+print(x<y)
+print(x<=y) # 2<=2
+print(x>=y) #5>=2
+
+x = 5
+print(not(x>3 or x>10) )# true
+
+a =5
+b=3
+print(a^b)
+x=5
+print(~x)
+x = 12
+print(x>>1)
+print(x>>2)
+
+
+
