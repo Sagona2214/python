3968e215d00a52df7eb86dcc3a8d5a58e71e6647
Sagona2214 <sagona2214@gmail.com>
2025-06-16 18:03:31 +0530

    Add files via upload

---
+++ Data types.py
@@ -0,0 +1,33 @@
+a=3
+print(type(a))
+b=3.14
+print(type(b))
+
+name="sagona"
+print(type(name))
+
+ison = True
+print(type(ison))
+
+a=1
+b="2"
+c=int(b)
+sum=(a+c)
+print(sum)
+
+
+#a=2 , b=2 & addition=(a+b) show the addition--
+a=2
+b=2
+addition=(a+b)
+print(addition)
+a=2.5
+b="5.5"
+#c=float(b)
+print(a+float(b))
+
+#a=34 b="56" c= 23 sub---
+a=34
+b="56"
+c=23
+print(a-int(b)-c)
